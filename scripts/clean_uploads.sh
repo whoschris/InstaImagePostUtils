4f56e8fa4c1111414751c6db85849ac75a48a468
Chris Hu <huchris96@gmail.com>
2022-02-21 21:11:39 -0800

    Fix scripts

--- scripts/clean_uploads.sh
+++ scripts/clean_uploads.sh
@@ -7,4 +7,5 @@ if [[ ${UPLOADS_DIR: -1} != "/" ]]; then
   UPLOADS_DIR=$UPLOADS_DIR"/"
 fi
 
+echo "Deleting photo uploads older than $MAX_OLDEST_DIR_MIN minutes"
 find $UPLOADS_DIR* -maxdepth 0 -mmin +$MAX_OLDEST_DIR_MIN -exec rm -rf {} \;
